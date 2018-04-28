e711d4b03801d85ea3b9eb341f0fd66b26e57ae5
miklax <mikla79@gmail.com>
2018-04-28 06:05:26 +0200

    code user settings

---
+++ code.js
@@ -0,0 +1,11 @@
+{
+    "editor.wordWrap": "on",
+    "workbench.colorTheme": "Default Light+",
+    "editor.fontFamily": "'input mono','Droid Sans Mono', 'Courier New', monospace, 'Droid Sans Fallback'",
+    "editor.fontSize": 12,
+    "editor.tabSize": 2,
+    "window.zoomLevel": 0,
+    "git.enableSmartCommit": true,
+    "git.confirmSync": false,
+    "git.autofetch": true
+}
